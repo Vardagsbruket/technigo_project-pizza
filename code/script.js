cee47b0c895aafda0667102529f4568bdb3adc6c
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-09-28 16:19:14 +0200

    add step 2 "food choice"

--- code/script.js
+++ code/script.js
@@ -1,23 +1,43 @@
 // Start here
 
 // Step 1 - Welcome and introduction
-// Your code goes here
+
 alert(
   `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
 )
-const name = prompt("Please tell me your name")
-alert("Hello " + name + "!" );
+const firstName = prompt("Please tell me your name")
+alert("Hello " + ${firstName} + "!" );
 
 // Step 2 - Food choice
-// Your code goes here
-let selectedFood = prompt("What would you like to order?" + [" Pizza", "pasta", "salad?"]);
 
+const foodChoice = prompt(
+  `What would you like to eat?
+  1 - Pizza
+  2 - Pasta
+  3 - Salad
+  Please enter the number of your choice.`
+);
+
+let selectedFood="";
+
+if(foodChoice==="1") {
+  selectedFood= "Pizza";
+} else if(foodChoice==="2") {
+  selectedFood = "Pasta";
+} else if (foodChoice==="3") {
+  selectedFood = "Salad";
+} else {
+  alert("Invalid choice, please start again.");
+  exit();
+}
+
+alert(`You have chosen ${selectedFood}.`);
 
 // Step 3 - Subtype choice
-// Your code goes here
+
 
 // Step 4 - Age
-// Your code goes here
+
 
 // Step 5 - Order confirmation
-// Your code goes here
+
